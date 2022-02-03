3d835c9c992d6d511bd83b73b69d8884e293215b
aliergarang <aliergarang16@gmail.com>
2022-02-03 18:27:59 +0300

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,30 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+    const tempInCelcius = (farenheit - 32) * 5 / 9;
+    if (!Number.isInteger(tempInCelcius)) {
+        return (+tempInCelcius.toFixed(1));
+    } else {
+        return tempInCelcius;
+    }
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+    const tempInFarenheit = celcius * 9 / 5 + 32;
+    if (!Number.isInteger(tempInFarenheit)) {
+        return (+tempInFarenheit.toFixed(1));
+    } else {
+        return tempInFarenheit;
+    }
 };
+ftoc(32);
+ftoc(100);
+ftoc(-100);
+ctof(0);
+ctof(73.2);
+ctof(-10);
+
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
